e322d8981a2807ff613046a9831ed264b08d46ed
Ravipati-Mounika <ravipatimounikaravipati@gmail.com>
2026-07-28 10:39:56 +0000

    html done

---
+++ js/test.js
@@ -0,0 +1,35 @@
+console.log("Hello World!")
+let student_name="MOUNIKA"
+console.log("Student name is:"+student_name)
+let student_age=21
+let course_name="Python Full stack"
+console.log("student age is:"+student_age,"\nCourse name is: "+course_name)
+let fee=10000
+let discount =10
+let discounted_fee=fee-(fee*discount/100)
+console.log("Discounted fe is:"+discounted_fee)
+let age=18
+if(age>=18){
+    console.log("Eligible for admission")
+} 
+else{
+    console.log("Not eligible for admission")
+}
+for (let i=1;i<=5;i++)
+{
+    console.log("Iteration nuber:"+i)
+}
+const pi=3.14
+console.log("Value of pi is"+pi)
+
+let student={
+    name:"Mounika",
+    age:21,
+    course:"python fullstack",
+    fee:10000
+}
+console.log("Student details:",student)
+function greetStudent(name){
+    console.log("Hello"+name+",Welcome to NRIIT Learning Management System")
+}
+console.log(student)
