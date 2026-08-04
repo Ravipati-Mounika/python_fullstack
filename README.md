5d2b78727f5cd9260c8a445ba9bb165b932b4e7b
Ravipati-Mounika <ravipatimounikaravipati@gmail.com>
2026-08-04 09:26:43 +0000

    folder done

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # python_fullstack
 html-hyper
-//DNS-domain name server
+DNS-domain name server
 Api=application programming interface->stand b/w frontend and backend
 middelware-stands as security check for request and response
 authorization vs authentication
@@ -12,3 +12,10 @@ orm-object relational mapping ,it is a techinque that allows you to quer and man
 pip install flask in env
 pip freeze > requirements.txt  
 pip freeze -r requirements.txt  
+check your ev activate
+no such file directory 
+python -m venv env
+python3 app.py---no module flask
+. env/bin/activate
+pip install flask
+python3 app.py
--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from flask import Flask
+from flask import Flask,render_template
 app=Flask(__name__)
 @app.route('/') #homepage server room
 def home():
-    return "Hello, World!"
+    return render_template("index.html")
+    #return "<h1>Mounika</h1>"
+@app.route("/about")
+def about():
+    return render_template("about.html")  
+
+@app.route("/contact")
+def contant():
+    return render_template("contant.html")
+
+@app.route("/courses")
+def courses():
+    return render_template("courses.html") 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/trainers")
+def trainers():
+    return render_template("trainers.html")    
 if __name__=='__main__':
     app.run(debug=True)    
